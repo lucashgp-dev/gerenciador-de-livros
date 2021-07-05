40129a62d03da904ff70fe3190e3b80a104a0b96
lucashgp-dev <lucashgp.l@hotmail.com>
2021-07-05 20:45:28 -0300

    projeto finalizado

---
+++ app/src/main/java/br/imd/fic/gerenciadordelivros/dominio/Livro.java
@@ -0,0 +1,72 @@
+package br.imd.fic.gerenciadordelivros.dominio;
+
+import java.io.Serializable;
+
+public class Livro implements Serializable {
+
+    //Atributos
+    private Long id;
+    private String titulo;
+    private String autor;
+    private String editora;
+    private int emprestado;
+
+    //Construtor
+
+    public Livro(Long id, String titulo, String autor, String editora, int emprestado) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editora = editora;
+        this.emprestado = emprestado;
+    }
+
+    public Livro(String titulo, String autor, String editora, int emprestado) {
+
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editora = editora;
+        this.emprestado = emprestado;
+    }
+
+    //Gets e Sets
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public int getEmprestado() {
+        return emprestado;
+    }
+    public void setEmprestado(int emprestado) {
+        this.emprestado = emprestado;
+    }
+}
